723a4b1897b59d14ab8cded71bca702d3d73e9bb
Innocent Ngene <ngeneinnocent@gmail.com>
2018-03-06 13:50:16 +0100

    made minor changes to home page

--- public/app/scripts/directives/carouselDirective.js
+++ public/app/scripts/directives/carouselDirective.js
@@ -6,42 +6,74 @@ function carousel(dataService) {
         templateUrl: 'app/views/directiveViews/carousel.html',
         link: function($scope, element, attrs) {
             var figures = $('#carousel figure');
-            function slide(array, index, sign1, sign2, nextLeft, nextCss, nextImage) {
-                $(array[index]).animate({
-                    left: (sign1 + '=67%'),
+            function slide(slideObject, index, nextImage) {
+                $(slideObject.array[index]).animate({
+                    left: (slideObject.sign1 + slideObject.leftAnimate),
                     opacity: 0
                 }, 500, function() {
-                    $(array[index])
+                    $(slideObject.array[index])
                     .removeClass('vissible')
-                    .css({'left': ('33.5%'), 'opacity': '1'});
+                    .css({'left': slideObject.left, 'opacity': '1'});
                 });
                 setTimeout(function(){
-                    $(array[nextImage]).css({'opacity': '0', 'left': `${sign2}${nextCss}`})
+                    $(slideObject.array[nextImage]).css({'opacity': '0', 'left': `${slideObject.sign2}${slideObject.nextCss}`})
                     .addClass('vissible')
                     .animate({
                         opacity: 1,
-                        left: (`${sign1}=${nextLeft}`)
+                        left: (`${slideObject.sign1}=${slideObject.nextLeft}`)
                     }, 500);
                 }, 20)
             }
             function rotateImagesFront() {
+                var slideObject = {
+                    array: figures,
+                    sign1: '+',
+                    sign2: '-',
+                }
+                if($('.not-phone').is(':visible')) {
+                    slideObject.left = '33.5%';
+                    slideObject.leftAnimate = '=67%';
+                    slideObject.nextLeft = '100.5%';
+                    slideObject.nextCss = '67%';
+                } else {
+                    slideObject.left = '25%';
+                    slideObject.leftAnimate = '=50%';
+                    slideObject.nextLeft = '75%';
+                    slideObject.nextCss = '50%';
+                }
                 for(var i = 0; i < figures.length; i++){
                     if(figures[i].className == 'vissible' && i != figures.length - 1){
-                        slide(figures, i, '+', '-', '100.5%', '67%', (i + 1));
+                        slide(slideObject, i, (i + 1));
                         break;
                     } else if(figures[i].className == 'vissible' && i == figures.length - 1) {
-                        slide(figures, i, '+', '-', '100.5%', '67%', (0));
+                        slide(slideObject, i, (0));
                         break;
                     }
                 }
             }
             function rotateImagesBack() {
+                var slideObject = {
+                    array: figures,
+                    sign1: '-',
+                    sign2: '+',
+                }
+                if($('.not-phone').is(':visible')) {
+                    slideObject.left = '33.5%';
+                    slideObject.leftAnimate = '=67%';
+                    slideObject.nextLeft = '67%';
+                    slideObject.nextCss = '100.5%';
+                } else {
+                    slideObject.left = '25%';
+                    slideObject.leftAnimate = '=50%';
+                    slideObject.nextLeft = '50%';
+                    slideObject.nextCss = '75%';
+                }
                 for(var i = 0; i < figures.length; i++){
                     if(figures[i].className == 'vissible' && i != 0){
-                        slide(figures, i, '-', '+', '67%', '100.5%', (i - 1));
+                        slide(slideObject, i, (i - 1));
                         break;
                     } else if(figures[i].className == 'vissible' && i == 0) {
-                        slide(figures, i, '-', '+', '67%', '100.5%', (figures.length - 1));
+                        slide(slideObject, i, (figures.length - 1));
                         break;
                     }
                 }

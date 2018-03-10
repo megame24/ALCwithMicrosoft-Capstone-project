be40b9bd6f862aa0954cd98a71dac56f4cca21ad
Innocent Ngene <ngeneinnocent@gmail.com>
2018-03-10 17:12:14 +0100

    added loaders

--- public/app/scripts/app.js
+++ public/app/scripts/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ngRoute']);
+angular.module('app', ['ngRoute','angular-loading-bar', 'ngAnimate']);
 angular.module('app').config(['$routeProvider', '$locationProvider', routeConfig]);
 
 //route configuration

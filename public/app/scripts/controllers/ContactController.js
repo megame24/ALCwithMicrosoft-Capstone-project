eb9fdc184e4407a5d92e2525286472010f41289c
Innocent Ngene <ngeneinnocent@gmail.com>
2018-03-11 15:25:33 +0100

    built contact page

--- public/app/scripts/controllers/ContactController.js
+++ public/app/scripts/controllers/ContactController.js
@@ -2,5 +2,9 @@ angular.module('app').controller('ContactController', [ContactController]);
 
 function ContactController() {
     var contact = this;
-    contact.welcome = 'Welcome to the contact page!!';
+    contact.contact = function() {
+        var value = contact.details
+        var message = `${value.name} your feedback on ${value.select} with details: ${value.message}, have been recieved. Please check your email: ${value.email} with in 2 days for a reply from us. Thank you for shopping with Grocery Cloud.`
+        alert(message);
+    }
 }
